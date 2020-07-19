6f82d1fb536e3629c170debc29c22aaebfffc944
Leonardo M. De Ugarte R <leonardodeu@outlook.com>
2020-07-19 16:29:11 -0700

    Initial Commit Some Mods

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Quote from './components/Quote.svelte';
+</script>
+
+<main>
+	<Quote/>
+</main>
+
+<style>
+	:global(body) {
+		height: 100vh;
+		display: grid;
+		place-content: center;
+		background-color: #633F49;
+	}
+
+	main {
+		max-width: 90%;
+		width: 720px;
+		background-color: #69444E;
+		box-shadow: 0 0 6px #1C1013;
+		border-radius: 5px;
+
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: repeat(3, auto);
+	}
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
