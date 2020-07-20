f8241b5ffe7a448fc533a362e787d9f10b3f1d51
Leonardo M. De Ugarte R <leonardodeu@outlook.com>
2020-07-19 17:03:30 -0700

    Add Quote Animation

--- src/components/Quote.svelte
+++ src/components/Quote.svelte
@@ -1,11 +1,30 @@
 <script>
+import { onMount } from 'svelte';
+import { slide } from 'svelte/transition';
+import { quintOut } from 'svelte/easing';
 
+let quote;
+onMount( () => {
+  setTimeout( () => {
+    state = true;
+  }, 1000);
+});
+
+let state = false;
+function refresh(){
+  state = false;
+  setTimeout( () => {
+    state = true;
+  }, 500);
+};
 </script>
 
-<div class="quote-container">
+{#if state}
+<div bind:this={quote} transition:slide="{{delay: 250, duration: 300, easing: quintOut}}" class="quote-container">
   <blockquote>There is nothing permanent except change.</blockquote>
   <p>Heraclitus</p>
 </div>
+{/if}
 
 <div class="icon-container">
   <button>
@@ -16,7 +35,7 @@
 </div>
 
 <div class="icon-container right-align">
-  <button>
+  <button on:click={refresh}>
     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="redo" class="svg-inline--fa fa-redo fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path fill="currentColor" d="M500.33 0h-47.41a12 12 0 0 0-12 12.57l4 82.76A247.42 247.42 0 0 0 256 8C119.34 8 7.9 119.53 8 256.19 8.1 393.07 119.1 504 256 504a247.1 247.1 0 0 0 166.18-63.91 12 12 0 0 0 .48-17.43l-34-34a12 12 0 0 0-16.38-.55A176 176 0 1 1 402.1 157.8l-101.53-4.87a12 12 0 0 0-12.57 12v47.41a12 12 0 0 0 12 12h200.33a12 12 0 0 0 12-12V12a12 12 0 0 0-12-12z"></path>
     </svg>
@@ -37,7 +56,6 @@
   text-align-last: right;
   padding-right: 2em;
  }
-
 .icon-container {
   display: flex;
   grid-row: 2 / 3;
